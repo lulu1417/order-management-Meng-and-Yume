f11bfa66c72d1a3973b1ed0b1dcd886bea4cf073
lulu1417 <baopeng@17.media>
2024-06-07 23:33:43 +0800

    create orderItem model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -13,4 +14,9 @@ class Order extends Model
       'no',
       'buyer_name'
     ];
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class, 'order_id');
+    }
 }
---
+++ database/migrations/2024_06_07_151714_create_order_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->string('name')->comment('商品名稱');
+            $table->string('count')->comment('商品數量');
+            $table->float('price')->comment('商品售價');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
